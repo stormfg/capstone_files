95b10453029c940a59eec0fabb0f5b98db1bf75e
Friedrich Gruber <stormfg@uw.edu>
2022-02-26 16:26:00 -0800

    Testing

--- kombu.py
+++ kombu.py
@@ -13,17 +13,10 @@
 import import_test
 import runpy
 import distance
+import time
 
 def main():
-    
-    userIn = input("Use default settings? [Y/N]: ")
-    print(userIn)
-
-    # if (userIn == 'N' or 'n'):
-    #         #ask input variables
-    #         print("wtf")
-    if(userIn == 'Y' or 'y'):
-        import_test.test()
+        
         distance.runLaser()
 
 if __name__=="__main__":
