3ffce8e0bf28abecae33ddfe6923e7fe874253d4
Friedrich Gruber <stormfg@uw.edu>
2022-02-27 20:28:18 -0800

    Skeleton code for alarm system

---
+++ alarmsys.py
@@ -0,0 +1,31 @@
+#!usr/bin/python
+
+class alarms():
+    def __init__(self):
+        self.ph_low = 2.5
+        self.ph_high = 3.5
+        self.abv_high = 2.0
+        self.temp_low = 68
+        self.temp_high = 78
+
+    def turn_on_horn(self):
+        print("noise")
+        return False
+    
+    def check_readings(self,abv,ph,temp):
+        if abv >= self.abv_high:
+            print("High ABV detected")
+            return self.turn_on_horn()
+        if ph >= self.ph_high:
+            print("High pH detected")
+            return self.turn_on_horn()
+        if ph <= self.ph_low:
+            print("Low pH detected")
+            return self.turn_on_horn()
+        if temp >= self.temp_high:
+            print("High Temp. detected")
+            return self.turn_on_horn()
+        if temp <= self.temp_low:
+            print("Low Temp. detected")
+            return self.turn_on_horn() 
+
