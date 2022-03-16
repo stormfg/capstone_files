398621e91fee4553baa05377f8478faa946980d6
Friedrich Gruber <stormfg@uw.edu>
2022-03-15 21:30:25 -0700

    Try to run everything on the LCD

--- kombu.py
+++ kombu.py
@@ -7,33 +7,40 @@
 #     if yes run
 #     if no   
 #         get user input for parameters
+#     obtain readings, use decorators?
+#     check readings to alarm setpoints
+#     if alarm setpoints passed activate LED
+#     user resets system, setpoints are checked right away
+#    
+#     initialization:
+#       distance,temp,pH 
 
-#     run distance.py 
-
+from cmath import phase
 import import_test
 import runpy
-#import distance
 import time
 import alarmsys
+import distance
+import Final_Code_maybe
 
 
-def obtain_readings():
-    #abv = distance.runLaser()
-    temp = 0
-    ph = 0
-    # test readings against alarm setpoints 
-    ala = alarmsys.alarms()
-    running = ala.check_readings(abv,ph,temp)
-
-running = True
+def check_readings(scale_obj):
+    def obtain_readings():
+        abv = distance.runLaser(scale_obj)
+        temp = round(sensor.get_temperature(),2)
+        pH = round(sensor.get_ph(),1)
+        alarmsys.alarms(abv,temp,pH)
+        return abv,temp,pH
+    return obtain_readings()
 
 def main():
-    print(obtain_readings())
-
-        # while running:
-        #     obtain_readings()
-        #     
-        #     time.sleep(600)
+    #zerod_scale = distance.initialize()
+    # while running or buttonState == True:
+    #     batch_info = check_readings(zerod_scale)
+    #     curr_abv = batch_info[0]
+    Final_Code_maybe()
+    
+    time.sleep(10)
 
 
 if __name__=="__main__":
